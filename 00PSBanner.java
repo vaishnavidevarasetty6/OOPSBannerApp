0fdcabb2809fc1615ee78b795ea75cb96fbb1c03
vaishnavidevarasetty6 <vaishnavidevarasetty6@gmail.com>
2026-02-17 20:36:09 +0530

    Add files via upload

---
+++ 00PSBanner.java
@@ -0,0 +1,12 @@
+public class OOPSBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *   *   **   ** ");
+        System.out.println("*   * *   *  *   *  *    ");
+        System.out.println("*   * *   *  **    * ");
+        System.out.println("*   * *   *  *          *");
+        System.out.println(" *   *   *      ** ");
+    }
+}
+   
